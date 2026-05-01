899a14cd16c9fa3a0f1c18f127434ba75253ba09
S.K.Obidul kadir <rezwankadirobaidul@gmail.com>
2026-05-01 10:34:31 +0600

    set the bearer token in suite label

--- src/test/java/api/tests/AuthTests.java
+++ src/test/java/api/tests/AuthTests.java
@@ -4,6 +4,7 @@ import static org.hamcrest.CoreMatchers.anyOf;
 import static org.hamcrest.CoreMatchers.is;
 
 import org.testng.Assert;
+import org.testng.ITestContext;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -44,7 +45,7 @@ public class AuthTests {
 	}
 
 	@Test(priority = 2 , dependsOnMethods = {"testPostUser"})
-	public void testPostLoginUser() {
+	public void testPostLoginUser(ITestContext context) {
 
 		Response response = AuthEndpoints.login_user(authPayload);
 
@@ -53,8 +54,9 @@ public class AuthTests {
 		Assert.assertEquals(response.getStatusCode(), 200);
 		Assert.assertEquals(response.jsonPath().getBoolean("success"), true);
 		bearer_token = response.jsonPath().getString("token");
+		context.getSuite().setAttribute("bearer_token", bearer_token);
 		
-		System.out.println("The bearer token after post : "+bearer_token);
+		System.out.println("The bearer token after login : "+bearer_token);
 		
 	}
 	
